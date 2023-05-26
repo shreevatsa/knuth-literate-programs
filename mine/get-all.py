9962f756bfd284cea2e5923ed27af10180050ae6
Shreevatsa R <shreevatsa.public@gmail.com>
2023-05-26 13:26:27 -0700

    Move the supporting files (not part of the initial download) into a separate directory

--- mine/get-all.py
+++ mine/get-all.py
@@ -1,4 +1,4 @@
-"""Download all programs and generate stats."""
+"""Download all CWEB programs from Knuth's webpage."""
 
 import os.path
 import requests
@@ -7,17 +7,15 @@ from bs4 import BeautifulSoup
 
 prefix = 'https://cs.stanford.edu/~knuth/'
 
-print('Getting the programs.html page to parse links.')
+print('Getting the programs.html page, to parse links.')
 response = requests.get(prefix + 'programs.html')
-
-soup = BeautifulSoup(response.content, 'html.parser')
-links = soup.find_all('a')
+links = BeautifulSoup(response.content, 'html.parser').find_all('a')
 
 for link in links:
     href = link.get('href')
     if not href:
         # Something that happens to be true for this page: non-links are names.
-        assert list(link.attrs.keys()) == ['name'], (link, link.attrs.keys())
+        assert list(link.attrs.keys()) == ['name']
         continue
     # Ignore links to other html pages
     if href.endswith('.html'):
@@ -30,20 +28,13 @@ for link in links:
     if href in ['words.tgz', 'ulam-gibbs.ps']:
         filename = href
     else:
-        (programs, filename) = href.split('/')
-        assert programs == 'programs'
+        assert href.startswith('programs/')
+        filename = href[len('programs/'):]
 
     url = prefix + href
-    if os.path.isfile(filename):
-        if open(filename, 'rb').read():
-            print(f'Already have {filename} from {url}')
-            continue
-        os.remove(filename)
-
-    # with open(filename, 'wb') as f:
-    #     print 'Downloading %s into %s' % (url, filename)
-    #     f.write(requests.get(url).content)
-    print(f'Downloading from {url}')
-    subprocess.call(['wget', url])
+    print(f'Downloading from {url} if updated.')
+    subprocess.call(['wget', '-N', url])
     if filename.endswith('.gz'):
-        subprocess.call(['gunzip', '-k', filename])
+        subprocess.call(['gunzip', '-k', '-f', filename])
+    if filename in ['tictactoe.tgz']:
+        subprocess.call(['tar', 'xvfz', filename])
